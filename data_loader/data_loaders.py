cb1e7fc387bd348d131d159e174ea068c7bf6615
hsuehkuan-lu <s810142000@gmail.com>
2020-10-11 18:16:01 +0800

    update data_loader

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,12 @@
+import re
+import os
+import csv
+import codecs
 from torchvision import datasets, transforms
 from base import BaseDataLoader
+
+MOVIE_LINES_FIELDS = ["lineID", "characterID", "movieID", "character", "text"]
+MOVIE_CONVERSATIONS_FIELDS = ["character1ID", "character2ID", "movieID", "utteranceIDs"]
 
 
 class MnistDataLoader(BaseDataLoader):
@@ -14,3 +21,86 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class ChatbotDataLoader(BaseDataLoader):
+    """
+    Chatbot data loading
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        self.data_dir = data_dir
+        lines = self._load_lines()
+        conversations = self._load_conversations(lines)
+        self._dump_file(conversations)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+    def _load_lines(self):
+        lines = {}
+        filename = os.path.join(self.data_dir, 'movie_lines.txt')
+        with open(filename, 'r', encoding='iso-8859-1') as f:
+            for line in f:
+                values = line.split(' +++$+++ ')
+                fields = {}
+                for idx, field in enumerate(MOVIE_LINES_FIELDS):
+                    fields[field] = values[idx]
+                lines[fields['lineID']] = fields
+        return lines
+
+    def _load_conversations(self, lines):
+        conversations = []
+        filename = os.path.join(self.data_dir, 'movie_conversations.txt')
+        with open(filename, 'r', encoding='iso-8859-1') as f:
+            for line in f:
+                values = line.split(' +++$+++ ')
+                # Extract fields
+                fields = {}
+                for i, field in enumerate(MOVIE_CONVERSATIONS_FIELDS):
+                    fields[field] = values[i]
+                # Convert string to list (convObj["utteranceIDs"] == "['L598485', 'L598486', ...]")
+                utterance_id_pattern = re.compile('L[0-9]+')
+                line_ids = utterance_id_pattern.findall(fields['utteranceIDs'])
+                # Reassemble lines
+                fields['lines'] = []
+                for line_id in line_ids:
+                    fields['lines'].append(lines[line_id])
+                conversations.append(fields)
+        return conversations
+
+    @staticmethod
+    def _extract_sentence_pairs(conversations):
+        qa_pairs = []
+        for conversation in conversations:
+            # Iterate over all the lines of the conversation
+            for i in range(len(conversation["lines"]) - 1):  # We ignore the last line (no answer for it)
+                input_line = conversation["lines"][i]["text"].strip()
+                target_line = conversation["lines"][i + 1]["text"].strip()
+                # Filter wrong samples (if one of the lists is empty)
+                if input_line and target_line:
+                    qa_pairs.append([input_line, target_line])
+        return qa_pairs
+
+    def _dump_file(self, conversations):
+        filename = os.path.join(self.data_dir, 'formatted_movie_lines.txt')
+        delimiter = '\t'
+        # Unescape the delimiter
+        delimiter = str(codecs.decode(delimiter, "unicode_escape"))
+
+        with open(filename, 'w', encoding='utf-8') as o:
+            writer = csv.writer(o, delimiter=delimiter, lineterminator='\n')
+            for pair in self._extract_sentence_pairs(conversations):
+                writer.writerow(pair)
+
+        self.print_lines(filename)
+
+    @staticmethod
+    def print_lines(filename, n=10):
+        with open(filename, 'rb') as datafile:
+            lines = datafile.readlines()
+        for line in lines[:n]:
+            print(line)
+
+
+data_loader = ChatbotDataLoader(
+    '/Users/hlu/Documents/Program/Git/chatbot/data/cornell movie-dialogs corpus',
+    batch_size=16
+)
